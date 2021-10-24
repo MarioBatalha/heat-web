90043ebdf17ddeebbbecf2a5b5993a67cb844e3a
MarioBatalha <mariobatalha9@gmail.com>
2021-10-24 01:40:02 +0100

    add handleSignIn

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,14 +1,50 @@
-import { VscGithubInverted } from 'react-icons/vsc';
-import styles from './styles.module.scss'; 
+import { useEffect } from "react";
+import axios from "axios";
+import { VscGithubInverted } from "react-icons/vsc";
+import { api } from "../../services/api";
+import styles from "./styles.module.scss";
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  };
+};
 
 export const LoginBox = () => {
-    return (
-        <div className={styles.loginBoxWrapper}>
-            <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
-                <VscGithubInverted size={24} />
-                Entrar com Github
-            </a>
-        </div>
-    )
-}
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=102df24cb3b6bae0e8ac`;
+
+  const handleSignIn = async (githubCode: string) => {
+    const response = await api.post<AuthResponse>("/authenticate", {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem("@dowhile:token", token);
+  };
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes("?code=");
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split("?code=");
+
+      window.history.pushState({}, "", urlWithoutCode);
+      handleSignIn(githubCode);
+    }
+  }, []);
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size={24} />
+        Entrar com Github
+      </a>
+    </div>
+  );
+};
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,49 +1,49 @@
-import { useEffect } from 'react';
-import styles from './style.module.scss';
-import Logo from '../../assets/logo.svg';
+import { useState, useEffect } from "react";
+import styles from "./style.module.scss";
+import Logo from "../../assets/logo.svg";
 
-import { api } from '../../services/api';
+import { api } from "../../services/api";
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
 
 export const MessageList = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
 
-    useEffect(() => {
-        api.get('messages/last3').then(response => {
-            console.log(response.data);
-        })
-    }, [])
-    return (
-        <div className={styles.messageListWrapper}>
-            <img src={Logo} alt="DoWHile 2021" />
-
-            <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamo que vamo!</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/mariobatalha.png" alt="Mário Batalha" />
-                        </div>
-                        <span>Mário Batalha</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamo que vamo!</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/mariobatalha.png" alt="Mário Batalha" />
-                        </div>
-                        <span>Mário Batalha</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamo que vamo!</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/mariobatalha.png" alt="Mário Batalha" />
-                        </div>
-                        <span>Mário Batalha</span>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    )
-}
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={Logo} alt="DoWHile 2021" />
+      <ul className={styles.messageList}>
+        {messages.map(messages => {
+          return (
+            <li className={styles.message} key={messages.id}>
+              <p className={styles.messageContent}>
+               {messages.text}
+              </p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img
+                    src={messages.user.avatar_url}
+                    alt={messages.user.name}
+                  />
+                </div>
+                <span>{messages.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
